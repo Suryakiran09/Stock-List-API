6eea06ffc4cffdb3b11e2a4c96209fdb41bde5fe
Surya kiran <123052451+Suryakiran09@users.noreply.github.com>
2024-05-16 07:45:41 +0530

    Updated settings.py

--- stock_app/settings.py
+++ stock_app/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'drf_yasg',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -52,6 +53,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -60,6 +62,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:*',
 ]
 
 ROOT_URLCONF = 'stock_app.urls'
