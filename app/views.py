aed61c26e9ba331b1f6a62da86b413f94bc22179
Suryakiran09 <starshipsn09@gmail.com>
2024-05-16 09:01:46 +0530

    Added all files

--- app/views.py
+++ app/views.py
@@ -46,7 +46,7 @@ class WatchListDetailView(generics.RetrieveUpdateDestroyAPIView):
         return WatchList.objects.filter(user=self.request.user)
 
 def get_stock_data(symbols):
-    base_url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&interval=5min&apikey=PYF2GF7RZALIZECI&symbol='
+    base_url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&interval=5min&apikey=P3Z7LTXRP46PHSY6&symbol='
     stock_data = defaultdict(lambda: {'data': []})
 
     for symbol in symbols:
